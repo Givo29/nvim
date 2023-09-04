5bfe669d774eb69f8d99eb05ee2e793cee21404b
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2023-09-04 15:01:18 +1000

    Disable treesitter auto_install

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require("nvim-treesitter.configs").setup({
 	ensure_installed = { "javascript", "typescript", "python", "lua", "rust" },
 	sync_install = false,
-	auto_install = true,
+	auto_install = false,
 	highlight = {
 		enable = true,
 		additional_vim_regex_highlighting = false,
