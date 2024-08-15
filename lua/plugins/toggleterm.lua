43eb82f192ae293f955dd083bc0ddc19ad3969b5
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-08-15 21:09:00 +1000

    Add toggleterm

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,29 @@
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+        local status_ok, toggleterm = pcall(require, "toggleterm")
+
+        if not status_ok then return end
+
+        toggleterm.setup({
+            size = 20,
+            open_mapping = [[<C-\>]],
+            hide_numbers = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            insert_mappings = true,
+            persist_size = true,
+            direction = "float",
+            shell = vim.o.shell,
+            float_opts = {
+                border = "curved",
+                winblend = 0,
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                }
+            }
+        })
+    end,
+}
