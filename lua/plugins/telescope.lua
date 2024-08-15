f8dbbdf5aac1826da5c3be0d91c3a72a591e3c0c
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-08-16 09:00:05 +1000

    Edit some keymappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,10 @@ return {
         local builtin = require("telescope.builtin")
 
         vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-        vim.keymap.set("n", "<C-p", builtin.git_files, {})
+        vim.keymap.set("n", "<leader>pg", builtin.git_files, {})
         vim.keymap.set("n", "<leader>ps", function()
             builtin.grep_string({ search = vim.fn.input("Search > ") })
         end)
-        vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+        vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
     end,
 }
