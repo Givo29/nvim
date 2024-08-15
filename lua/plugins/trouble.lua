1496c16936a483ce64285bcc3f3558a80e727b65
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2024-08-16 09:01:50 +1000

    Add trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,26 @@
+return {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>t",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>T",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>ts",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>tq",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
+    },
+}
